1d18cafa7181552c9fa9ad4c5d053e4d3cb5579e
jeisaRaja <jeisa.raja.jrt@gmail.com>
2025-01-17 03:25:40 +0700

    rgb color triangle

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,30 @@
 #include "../include/glad/glad.h"
 #include <GL/gl.h>
 #include <GLFW/glfw3.h>
+#include <bits/types/struct_timeval.h>
+#include <cmath>
 #include <iostream>
 
 unsigned int vertexShader, fragmentShader, shaderProgram;
 int success;
 char infoLog[512];
 const char *fragmentShaderSource = "#version 330 core\n"
-                                   "out vec4 FragColor;"
+                                   "out vec4 FragColor;\n"
+                                   "in vec3 ourColor;\n"
                                    "void main()\n"
                                    "{\n"
-                                   " FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);"
+                                   " FragColor = vec4(ourColor, 1.0);\n"
                                    "}\0";
 
-const char *vertexShaderSource =
-    "#version 330 core\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "void main()\n"
-    "{\n"
-    " gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-    "}\0";
+const char *vertexShaderSource = "#version 330 core\n"
+                                 "layout (location = 0) in vec3 aPos;\n"
+                                 "layout (location = 1) in vec3 aColor;\n"
+                                 "out vec3 ourColor;\n"
+                                 "void main()\n"
+                                 "{\n"
+                                 " gl_Position = vec4(aPos, 1.0);\n"
+                                 " ourColor = aColor;\n"
+                                 "}\0";
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
   glViewport(0, 0, width, height);
@@ -53,10 +58,14 @@ int main() {
   glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
   float vertices[] = {
-      -0.5f, 0.5f,  0.0f, // top-left
-      0.5f,  0.5f,  0.0f, // top-right
-      0.5f,  -0.5f, 0.0f, // bottom-right
-      -0.5f, -0.5f, 0.0f  // bottom-left
+      -0.5f, 0.5f,  0.0f, 1.0f, 0.0f, 0.0f, // top-left
+      0.5f,  0.5f,  0.0f, 0.0f, 1.0f, 0.0f, // top-right
+      0.5f,  -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, // bottom-right
+      /*-0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom-left*/
+      /*-0.8f, 0.5f,  0.0f, 0.0f, 1.0f, 0.0f, // top-left*/
+      /*0.5f,  0.5f,  0.0f, 0.0f, 0.0f, 1.0f, // top-right*/
+      /*0.5f,  -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom-right*/
+      /*-0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // bottom-left*/
   };
 
   unsigned int indices[] = {
@@ -77,9 +86,12 @@ int main() {
   glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
                GL_STATIC_DRAW);
 
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),
-                        ((void *)0));
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
   glEnableVertexAttribArray(0);
+
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float),
+                        (void *)(3 * sizeof(float)));
+  glEnableVertexAttribArray(1);
 
   glBindBuffer(GL_ARRAY_BUFFER, 0);
   glBindVertexArray(0);
@@ -123,7 +135,7 @@ int main() {
   }
 
   // Configure OpenGL to use line to draw primitive
-  glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+  glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
   while (!glfwWindowShouldClose(window)) {
     processInput(window);
@@ -131,15 +143,16 @@ int main() {
     glUseProgram(shaderProgram);
 
     glBindVertexArray(VAO);
-
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-
-    glBindVertexArray(0);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
 
     glfwSwapBuffers(window);
-
     glfwPollEvents();
   }
+
+  glDeleteVertexArrays(1, &VAO);
+  glDeleteVertexArrays(1, &VBO);
+  glDeleteVertexArrays(1, &EBO);
+  glDeleteProgram(shaderProgram);
   glfwTerminate();
   return 0;
 }
